61a1dbd9864836706754dbcfa5fea1fe706fcb44
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-08-30 10:48:04 +0300

    feat(component-data): format manifest.json and expand component registry with additional components

--- src/component_data/index.ts
+++ src/component_data/index.ts
@@ -76,97 +76,162 @@ export const componentRegistry: ComponentData = {
   CheckboxVector: {
     name: "Checkbox (Vector)",
     key: "43e9aef5432cf48a3cf2b727a815872f717ba211",
-    type: "componentSet"
+    type: "componentSet",
   },
   "Checkbox Item Vector": {
     name: "Checkbox Item (Vector)",
     key: "b07ff3f8009f606c7537098bfa932d1e916206ac",
-    type: "componentSet"
+    type: "componentSet",
   },
   Chips: {
     name: "Chips",
     key: "d785439063b42d9fbe449f3d19223cfa825a47bf",
-    type: "componentSet"
+    type: "componentSet",
   },
   "Text Input Outlined": {
     name: "Text Input (Outlined)",
     key: "3fa0112d53708a35080dfd22530ebf3dbbbcdf4d",
-    type: "componentSet"
+    type: "componentSet",
   },
   "Text Input Contained": {
     name: "Text Input (Contained)",
     key: "46e44149a998ceae1bdbff378c85818e4ecd89e6",
-    type: "componentSet"
+    type: "componentSet",
   },
   "Text Input Underlined": {
     name: "Text Input (Underlined)",
     key: "2ef5964e087304eaecb3887bb3b2441834450f21",
-    type: "componentSet"
+    type: "componentSet",
   },
   "Select Input Outlined": {
     name: "Select Input (Outlined)",
     key: "bb1ce670f2e5eb30645a8381232c4ab166a56834",
-    type: "componentSet"
+    type: "componentSet",
   },
-  'Select Input Contained': {
+  "Select Input Contained": {
     name: "Select Input (Contained)",
     key: "0aeeec1c853b6245284581b97e9f7c04f63e1f60",
-    type: "componentSet"
+    type: "componentSet",
   },
   "Select Input Underlined": {
     name: "Select Input (Underlined)",
     key: "cbba2e72a7ad213bef170e4856c5cc6d25f61025",
-    type: "componentSet"
+    type: "componentSet",
   },
   "Text Area Outlined": {
     name: "Text Area (Outlined)",
     key: "cd98d2840dfb806c6d4565cf350241e5154b77e6",
-    type: "componentSet"
+    type: "componentSet",
   },
-  'Text Area Contained': {
+  "Text Area Contained": {
     name: "Text Area (Contained)",
     key: "db19df1e8d74d68586027a3adedd40c52b242c26",
-    type: "componentSet"
+    type: "componentSet",
   },
   "Text Area Underlined": {
     name: "Text Area (Underlined)",
     key: "da1e71702ca3f2fa022054c190c88dfa36cb4d7e",
-    type: "componentSet"
+    type: "componentSet",
   },
   "Numeric Input (Arrows) Outlined": {
     name: "Numeric Input (Arrows) (Outlined)",
-    key: "",
-    type: "componentSet"
+    key: "f6875ee813fd19313ff01f17878ae1bfdf26f188",
+    type: "componentSet",
   },
-  'Numeric Input (Arrows) Contained': {
+  "Numeric Input (Arrows) Contained": {
     name: "Numeric Input (Arrows) (Contained)",
-    key: "",
-    type: "componentSet"
+    key: "707fc9732016ef5453e1f2bb6c80a484570ebf5e",
+    type: "componentSet",
   },
   "Numeric Input (Arrows) Underlined": {
     name: "Numeric Input (Arrows) (Underlined)",
-    key: "",
-    type: "componentSet"
+    key: "253d85ed1a71e4becb01a1678160ebc0f6480b47",
+    type: "componentSet",
   },
   "Numeric Input (Buttons) Outlined": {
     name: "Numeric Input (Buttons) (Outlined)",
-    key: "",
-    type: "componentSet"
+    key: "edafba6ef44ce824e754975cec86f294bc52c665",
+    type: "componentSet",
   },
-  'Numeric Input (Buttons) Contained': {
+  "Numeric Input (Buttons) Contained": {
     name: "Numeric Input (Buttons) (Contained)",
-    key: "",
-    type: "componentSet"
+    key: "f09ecd4fa12ca25558f7ee9fc62e78a7c2aeb163",
+    type: "componentSet",
   },
   "Numeric Input (Buttons) Underlined": {
     name: "Numeric Input (Buttons) (Underlined)",
-    key: "",
-    type: "componentSet"
+    key: "c16a73a74fc69dcbd69b8207f268f998a7ff69af",
+    type: "componentSet",
   },
-  "Numeric Input (Stepper)": {
-    name: "Numeric Input (Stepper)",
+  "Numeric Input (Stepper 1)": {
+    name: "Numeric Input (Stepper 1)",
+    key: "0c7481e296b1aa2fd6a9ee723e862b1fb1bb0879",
+    type: "componentSet",
+  },
+  "Numeric Input (Stepper 2)": {
+    name: "Numeric Input (Stepper 2)",
+    key: "920d6e95c1ffe44c033a66901f5328e088b31f36",
+    type: "componentSet",
+  },
+  "Radio Button (Icon)": {
+    name: "Radio Button (Icon)",
+    key: "d03856624a6be9ba50ce2bd04fc6df292ba35426",
+    type: "componentSet",
+  },
+  "Radio Button Item (Icon)": {
+    name: "Radio Button Item (Icon)",
+    key: "67b1272ac6d5fedbf7cba7f4e99224e463abe392",
+    type: "componentSet",
+  },
+  "Radio Button (Vector)": {
+    name: "Radio Button (Vector)",
+    key: "217f967a6f658672c005979c9bc388811c3eeeb4",
+    type: "componentSet",
+  },
+  "Radio Button Item (Vector)": {
+    name: "Radio Button Item (Vector)",
+    key: "7f58a9fa0837ad52cf63ae52b08ed7de88c359bd",
+    type: "componentSet",
+  },
+  Link: {
+    name: "Link",
+    key: "7c987b097108e85d6b25ab2037f58013b41648da",
+    type: "componentSet",
+  },
+  Slider: {
+    name: "Slider",
+    key: "634edb92fa517ba797c48d12666af58dd4f72b1c",
+    type: "componentSet",
+  },
+  "Search (simple) Outlined": {
+    name: "Search (simple) Outlined",
     key: "",
-    type: "componentSet"
+    type: "componentSet",
+  },
+  "Search (simple) Contained": {
+    name: "Search (simple) Contained",
+    key: "",
+    type: "componentSet",
+  },
+  "Search (simple) Underlined": {
+    name: "Search (simple) Underlined",
+    key: "",
+    type: "componentSet",
+  },
+  "Search (with label and helper text) Outlined": {
+    name: "Search (simple) Outlined",
+    key: "",
+    type: "componentSet",
+  },
+  "Search (with label and helper text) Contained": {
+    name: "Search (simple) Contained",
+    key: "",
+    type: "componentSet",
+  },
+  "Search (with label and helper text) Underlined": {
+    name: "Search (simple) Underlined",
+    key: "",
+    type: "componentSet",
   },
 };
 
